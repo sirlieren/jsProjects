7ca85fce98db782c773cef50942c66f509e110a1
Eren Sirli <e3345ren@outlook.com>
2024-07-22 17:24:38 +0300

    CardSliderApp

---
+++ ResponsiveCardSlider/script.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.sliderWrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 15,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
